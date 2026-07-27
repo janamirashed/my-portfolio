afe2238191f336dc90843c2fcdde5c90bad870fc
janamirashed <janarashed23010359@gmail.com>
2026-07-27 19:25:10 +0300

    deploy browser tab favicon update to memoji avatar pic

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
-import { ChatComponent } from './pages/chat/chat.component';
-
-export const routes: Routes = [
-    { path: '', component: LandingComponent },
-    { path: 'chat', component: ChatComponent },
-    { path: '**', redirectTo: '' }
-];
